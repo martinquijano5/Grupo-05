1e13fc4945234a319c42879e898d5024d319c98b
Lola <lbellini@udesa.edu.ar>
2021-11-09 17:45:18 -0300

    querystring

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -1,2 +1,9 @@
 //traer detalle de series dinamicamente
 //serie https://api.themoviedb.org/3/tv/{tv_id}  
+
+
+//let section = document.querySelectorAll 
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let q = queryStringObj.get('q');
+console.log(q);
