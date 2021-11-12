52d7559d8391aeb3c7fa80ac3d25e3967f219c1c
Lola <lbellini@udesa.edu.ar>
2021-11-12 17:44:47 -0300

    fetch detalle series

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -2,8 +2,24 @@
 //serie https://api.themoviedb.org/3/tv/{tv_id}  
 
 
-//let section = document.querySelectorAll 
+let secciones = document.querySelectorAll ("section")
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let q = queryStringObj.get('q');
 console.log(q);
+
+fetch (`https://api.themoviedb.org/3/tv/${q}?api_key=66374e925f9ce0061d8e10191732f374`)
+.then(function(response) {
+    return response.json();
+})
+
+.then(function (data) {
+
+console.log(data)
+console.log(secciones)
+
+})
+
+.catch(function (error) {
+    console.log('el error fue ' + error);
+})
